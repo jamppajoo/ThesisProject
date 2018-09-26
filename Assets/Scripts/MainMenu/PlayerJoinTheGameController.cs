a295d596ff6c65241ceb07a8491785eb56fc147a
Jan Julin <mummo94@hotmail.com>
2018-09-26 17:15:35 +0300

    Main menu graphics added

---
+++ Assets/Scripts/MainMenu/PlayerJoinTheGameController.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerJoinTheGameController : MonoBehaviour
+{
+
+    private string myPlayerID;
+    private RectTransform playerJoinedGameImage;
+    private RectTransform joinTheGameText;
+
+    private bool playerHasJoinedGame = false;
+
+    void Start()
+    {
+        myPlayerID = gameObject.transform.name.Substring(0, 2);
+        joinTheGameText = gameObject.transform.GetChild(0).Find("JoinTheGameText").GetComponent<RectTransform>();
+        playerJoinedGameImage = gameObject.transform.GetChild(0).Find("PlayerChoosing").GetComponent<RectTransform>();
+    }
+
+    void Update()
+    {
+
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            if (!playerHasJoinedGame)
+                JoinTheGame();
+            else
+                LeaveTheGame();
+        }
+
+    }
+
+    private void JoinTheGame()
+    {
+        joinTheGameText.position = playerJoinedGameImage.position;
+        playerJoinedGameImage.localPosition = Vector3.zero;
+        playerHasJoinedGame = true;
+    }
+    private void LeaveTheGame()
+    {
+        playerJoinedGameImage.position = joinTheGameText.position;
+        joinTheGameText.localPosition = Vector3.zero;
+        playerHasJoinedGame = false;
+    }
+}
