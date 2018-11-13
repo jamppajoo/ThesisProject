636bdb41e6eba8619d521de5f6ee57f123103f89
Jan Julin <mummo94@hotmail.com>
2018-11-13 19:06:00 +0200

    Added decoration sprites, better spritemask system for color block

---
+++ Assets/Scripts/Editor/Brushes and Tiles/DecorationSpawnerBrush.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using UnityEditor.SceneManagement;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+namespace UnityEditor
+{
+    [CustomGridBrush(true, false, false, "Decoration Spawner Brush")]
+    public class DecorationSpawnerBrush : GridBrush
+    {
+        [MenuItem("Assets/Create/Decoration Spawner Brush")]
+        public static void CreateBrush()
+        {
+            string path = EditorUtility.SaveFilePanelInProject("Save Decoration Spawner Brush", "New Decoration Spawner Brush", "asset", "Save Decoration Spawner Brush", "Assets");
+
+            if (path == "")
+                return;
+
+            AssetDatabase.CreateAsset(ScriptableObject.CreateInstance<DecorationSpawnerBrush>(), path);
+        }
+        
+    }
+
+    [CustomEditor(typeof(DecorationSpawnerBrush))]
+    [CanEditMultipleObjects]
+    public class DecorationSpawnerBrushEditor : GridBrushEditor
+    {
+        private DecorationSpawnerBrush decorationSpawnerBrush { get { return target as DecorationSpawnerBrush; } }
+
+
+        Tilemap decorationTilemap;
+        Tilemap platformTilemap;
+        TileBase decorationTile;
+        public override void OnInspectorGUI()
+        {
+            EditorGUILayout.BeginHorizontal();
+            EditorGUILayout.LabelField("Decoration tilemap layer");
+            EditorGUILayout.LabelField("Platform tilemap layer");
+            EditorGUILayout.EndHorizontal();
+
+            EditorGUILayout.BeginHorizontal();
+            decorationTilemap = (Tilemap)EditorGUILayout.ObjectField(decorationTilemap, typeof(Tilemap), false);
+
+            platformTilemap = (Tilemap)EditorGUILayout.ObjectField(platformTilemap, typeof(Tilemap), false);
+
+            EditorGUILayout.EndHorizontal();
+
+            EditorGUILayout.LabelField("Decoration tile");
+
+            decorationTile = (TileBase)EditorGUILayout.ObjectField(decorationTile, typeof(TileBase), false);
+
+            if (GUILayout.Button("Fill Decorations"))
+            {
+                
+                Tilemap platformTilemapReference = GameObject.Find(platformTilemap.name).GetComponent<Tilemap>();
+                Tilemap decorationTilemapReference = GameObject.Find(decorationTilemap.name).GetComponent<Tilemap>();
+
+                for (int px = platformTilemapReference.cellBounds.xMin; px < platformTilemapReference.cellBounds.xMax; px++)
+                {
+                    for (int py = platformTilemapReference.cellBounds.yMin; py < platformTilemapReference.cellBounds.yMax; py++)
+                    {
+                        Vector3Int localPlace = (new Vector3Int(px, py, (int)platformTilemapReference.transform.position.y));
+                        Vector3 place = platformTilemapReference.CellToWorld(localPlace);
+                        if(platformTilemapReference.HasTile(localPlace))
+                        {
+                            decorationTilemapReference.SetTile(localPlace, null);
+                        }
+                    }
+
+                }
+
+
+
+            }
+        }
+    }
+}
--- Assets/Scripts/Level/TilemapSpriteMask.cs
+++ Assets/Scripts/Level/TilemapSpriteMask.cs
@@ -1,74 +1,100 @@
 ﻿using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Tilemaps;
+using System.Linq;
+using UnityEditor;
 
 [RequireComponent(typeof(Tilemap))]
 public class TilemapSpriteMask : MonoBehaviour
 {
     Tilemap t;
     public Sprite tileMask;
+    private List<GameObject> generatedSpriteMasks;
 
-    //void Start()
-    //{
-    //    t = GetComponent<Tilemap>();
-    //    BoundsInt bounds = t.cellBounds;
-
-    //    var s = t.layoutGrid.cellSize / 2;
-
-    //    var availablePlaces = new List<Vector3>();
-
-    //    for (int n = t.cellBounds.xMin; n < t.cellBounds.xMax; n++)
-    //    {
-    //        for (int p = t.cellBounds.yMin; p < t.cellBounds.yMax; p++)
-    //        {
-    //            Vector3Int localPlace = (new Vector3Int(n, p, (int)t.transform.position.y));
-    //            Vector3 place = t.CellToWorld(localPlace);
-
-    //            TileBase tile = t.GetTile(localPlace);
-    //            CompositeCollider2D compositeCollider = t.GetComponent<CompositeCollider2D>();
-    //            for (int i = 0; i < compositeCollider.pathCount; i++)
-    //            {
-    //                Vector2[] points = new Vector2[compositeCollider.GetPathPointCount(i)];
-    //                Debug.Log("ASD: " + compositeCollider.pathCount);
-
-    //            }
-
-    //            if (tile)
-    //            {
-    //                availablePlaces.Add(place);
-
-    //                var c = new GameObject().AddComponent<SpriteMask>();
-    //                c.GetComponent<SpriteMask>().sprite = tileMask;
-    //                c.transform.parent = t.transform;
-    //                c.transform.localPosition = t.CellToLocal(localPlace) + s;
-
-    //            }
-    //        }
-    //    }
-    //}
-
-    private void Start()
+    public void GenerateSpriteMask()
     {
-        CompositeCollider2D compositeCollider = GetComponent<CompositeCollider2D>();
-        for (int i = 0; i < compositeCollider.pathCount; i++)
+        DestroySpriteMasks();
+        t = GetComponent<Tilemap>();
+        BoundsInt bounds = t.cellBounds;
+
+        var s = t.layoutGrid.cellSize / 2;
+
+        var availablePlaces = new List<Vector3>();
+
+        for (int n = t.cellBounds.xMin; n < t.cellBounds.xMax; n++)
         {
-            Vector2[] points = new Vector2[compositeCollider.GetPathPointCount(i)];
-            Debug.Log("ASD: " + compositeCollider.GetPath(i, points));
-            Debug.Log("ASD: " + points[0] + points[1] + points[2] + points[3]);
+            for (int p = t.cellBounds.yMin; p < t.cellBounds.yMax; p++)
+            {
+                Vector3Int localPlace = (new Vector3Int(n, p, (int)t.transform.position.y));
+                Vector3 place = t.CellToWorld(localPlace);
 
-            GameObject newMaskObject = new GameObject();
-            SpriteMask newMask = newMaskObject.AddComponent<SpriteMask>();
-            newMask.sprite = tileMask;
-            
-            Vector2 localScale = new Vector2(points[0].x - points[1].x, points[1].y - points[2].y);
-            Vector2 localPosition = new Vector2(points[0].x - (points[0].x - points[1].x), 0);
+                TileBase tile = t.GetTile(localPlace);
+                //CompositeCollider2D compositeCollider = t.GetComponent<CompositeCollider2D>();
+                //for (int i = 0; i < compositeCollider.pathCount; i++)
+                //{
+                //    Vector2[] points = new Vector2[compositeCollider.GetPathPointCount(i)];
 
-            newMaskObject.transform.parent = gameObject.transform;
-            newMaskObject.transform.localScale = localScale;
-            newMaskObject.transform.localPosition = localPosition;
+                //}
 
+                if (tile)
+                {
+                    availablePlaces.Add(place);
 
+                    var c = new GameObject().AddComponent<SpriteMask>();
+                    c.GetComponent<SpriteMask>().sprite = tileMask;
+                    c.transform.parent = t.transform;
+                    c.transform.localPosition = t.CellToLocal(localPlace) + s;
+                    c.name = "SpriteMask";
+                    generatedSpriteMasks.Add(c.gameObject);
+
+                }
+            }
+        }
+    }
+    private void DestroySpriteMasks()
+    {
+        foreach (GameObject item in generatedSpriteMasks)
+        {
+            DestroyImmediate(item);
         }
     }
 
+    //private void Start()
+    //{
+    //    CompositeCollider2D compositeCollider = GetComponent<CompositeCollider2D>();
+    //    for (int i = 0; i < compositeCollider.pathCount; i++)
+    //    {
+    //        Vector2[] points = new Vector2[compositeCollider.GetPathPointCount(i)];
+    //        compositeCollider.GetPath(i, points);
+    //        Debug.Log("ASD: " + points[0] + points[1] + points[2] + points[3]);
+    //        Vector2[] newPoints = points.OrderBy(v => v.y).ToArray<Vector2>();
+
+    //        GameObject newMaskObject = new GameObject();
+    //        SpriteMask newMask = newMaskObject.AddComponent<SpriteMask>();
+    //        newMask.sprite = tileMask;
+
+    //        Vector2 localScale = new Vector2(Mathf.Abs(points[0].x - points[1].x), Mathf.Abs(points[1].y - points[2].y));
+    //        Vector2 localPosition = new Vector2(points[0].x - Mathf.Abs(points[0].x - points[1].x) /2, points[1].y - Mathf.Abs(points[1].y - points[2].y) / 2);
+
+    //        newMaskObject.transform.parent = gameObject.transform;
+    //        newMaskObject.transform.localScale = localScale;
+    //        newMaskObject.transform.localPosition = localPosition;
+
+
+    //    }
+    //}
+}
+[CustomEditor(typeof(TilemapSpriteMask))]
+[CanEditMultipleObjects]
+public class TilemapSpriteMaskEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        base.OnInspectorGUI();
+        if (GUILayout.Button("Generate tilemap sprite mask"))
+        {
+            TilemapSpriteMask tilemapSpriteMask = (TilemapSpriteMask)target;
+            tilemapSpriteMask.GenerateSpriteMask();
+        }
+    }
 }
